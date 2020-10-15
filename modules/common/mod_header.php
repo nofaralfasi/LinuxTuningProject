b68670153ece20635b46c154f323e2457455664c
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-15 17:53:43 +0300

    Add files via upload

--- modules/common/mod_header.php
+++ modules/common/mod_header.php
@@ -2,7 +2,7 @@
 	<a id="top"></a>
 	<div class='container'>
 		<div class='sixteen columns'>
-			<h1><a href="<?php echo $root; ?>">Linux Tuning Project - Nofar Alfasi</a></h1>
+			<h1><a href="<?php echo $root; ?>">Linux Tuning Project - By Nofar Alfasi</a></h1>
 		</div>
 	</div>
 </header>
