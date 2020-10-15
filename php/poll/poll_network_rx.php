20ae3e57e71a96fc601d7d69f2a1d42691bf145e
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-15 20:09:55 +0300

    Add files via upload

--- php/poll/poll_network_rx.php
+++ php/poll/poll_network_rx.php
@@ -1,5 +1,8 @@
 <?php
-	$result = round(trim(file_get_contents("/sys/class/net/".$config['network']['interface']."/statistics/rx_bytes")) / 1024/ 1024/ 1024, 2);
+	if(isSet($config['network']['interface'])){
+		$config['network']['interface'] = "ens192";
+	}
+	$result = round(trim(file_get_contents("/sys/class/net/" . $config['network']['interface'] . "/statistics/rx_bytes")) / 1024/ 1024/ 1024, 2);
 	echo $result;
 	return $result;
 ?>
