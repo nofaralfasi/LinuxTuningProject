82b4f7e0580b1b6547a2a97e9cfa6516b430d157
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-15 17:52:02 +0300

    Add files via upload

--- modules/common/mod_footer.php
+++ modules/common/mod_footer.php
@@ -2,11 +2,11 @@
 	<div class='container'>
 		<div class='eight columns'>
 			<h5>Configure</h5>
-			<p>Visit the <a href='<?php echo $root; ?>conf'>configuration</a> page to alter which modules are displayed by ServerStatus, or to modify the content of them.</p>
+			<p>Visit the <a href='<?php echo $root; ?>conf'>configuration</a> page to alter which modules are displayed, or to modify the content of them.</p>
 		</div>
 		<div class='eight columns'>
 			<h5>GitHub</h5>
-			<p>Visit the ServerStatus <a href='https://github.com/dan142/ServerStatus'>GitHub page</a> to access the documentation or to help by submitting improvements.</p>
+			<p>Visit my Linux Tuning Project <a href='https://github.com/nofaralfasi/LinuxTuningProject'>GitHub page</a>.</p>
 		</div>
 	</div>
 </footer>
