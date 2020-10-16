f56d94be2a5b114d5a347864b4af0dab91bc750c
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-17 00:35:47 +0300

    Add files via upload

--- php/poll/poll_ram_free.php
+++ php/poll/poll_ram_free.php
@@ -9,17 +9,11 @@
 	$mem = explode(" ", $free_arr[1]);
 	$mem = array_filter($mem, function($value) { return ($value !== null && $value !== false && $value !== ''); }); // removes nulls from array
 	$mem = array_merge($mem); // puts arrays back to [0],[1],[2] after 
-	$memtotal = round($mem[1] / 1000000,2);
-	$memused = round($mem[2] / 1000000,2);
+
 	$memfree = round($mem[3] / 1000000,2);
-	$memshared = round($mem[4] / 1000000,2);
-	$memcached = round($mem[5] / 1000000,2);
+
 	$memavailable = round($mem[6] / 1000000,2);
 
-	$memusage = round(($memavailable/$memtotal)*100);
-
-	$stat['ram_mem_total'] =  $memtotal;
-	$stat['ram_mem_used'] = $memused;
 	$stat['ram_mem_available'] = $memavailable;
 
 	echo $memavailable;
