8d31d258aa6210f19caf08c06452a9a5ac07f1d1
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-18 00:54:47 +0300

    Update poll_os_uptime.php

--- php/poll/poll_os_uptime.php
+++ php/poll/poll_os_uptime.php
@@ -1,5 +1,5 @@
 <?php
-	$uptime = shell_exec("cut -d. -f1 /proc/uptime");
+	$uptime = shell_exec("cut -d. -f 1 /proc/uptime");
 	$days = floor($uptime/60/60/24);
 	$hours = $uptime/60/60%24;
 	$mins = $uptime/60%60;
