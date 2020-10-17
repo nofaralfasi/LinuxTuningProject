9fb95c290c5db44af776ee20193b7704ea78ae52
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-18 00:23:28 +0300

    Add files via upload

--- php/poll/poll_cpu_usage.php
+++ php/poll/poll_cpu_usage.php
@@ -5,8 +5,8 @@
 	//echo "<script>console.log('prevVal Objects: " . $prevArr[2] . "' );</script>";
 	$prevTotal = $prevArr[2] + $prevArr[3] + $prevArr[4] + $prevArr[5] + $prevArr[6] + $prevArr[7] + $prevArr[8];
 	$prevIdle = $prevArr[5];
-	usleep(0.15 * 1000000);
-	//sleep(1);
+	//usleep(0.15 * 1000000);
+	sleep(1);
 	$val = shell_exec("./../../scripts/cat2 /proc/stat");
 	$arr = explode(' ', trim($val));
 	$total = $arr[2] + $arr[3] + $arr[4] + $arr[5] + $arr[6] + $arr[7] + $arr[8];
