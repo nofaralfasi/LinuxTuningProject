6dd44706204fe8fbf01c44c6991d98caec8d83e0
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-22 19:20:47 +0300

    Add files via upload

--- php/poll/poll_cpu_model.php
+++ php/poll/poll_cpu_model.php
@@ -1,13 +1,12 @@
 <?php
-	//$cpu_result1 = shell_exec("cat /proc/cpuinfo | grep model\ name");
-	$cpu_result = shell_exec("./../../scripts/grep name /proc/cpuinfo");
-
-	//$cpu_result = substr($cpu_result, 0);
-	//$cpu_result = explode("\n", $cpu_result);
-
-	//$output = strstr($cpu_result, "\n", true);
-	//$result = str_replace("model name	: ", "", $output);
-	//$my_output = $v;
+	//$cpu_result2 = shell_exec("cat /proc/cpuinfo | grep model\ name");
+	$cpu_result = shell_exec("./scripts/grep name /proc/cpuinfo");
+	$cpu_result = strstr($cpu_result, "\n", true);
+	$cpu_result = str_replace("model name	: ", "", $cpu_result);
+	$my_output = $cpu_result;
 	echo $cpu_result;
 	return $cpu_result;
 ?>
+
+
+
