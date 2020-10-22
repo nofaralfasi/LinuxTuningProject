5c55fa0877a0c71c34fd7f4c13a825ab051fb6f1
nofar <nofar.alfasi@s.afeka.ac.il>
2020-10-22 18:21:40 +0300

    Add files via upload

--- scripts/grep.c
+++ scripts/grep.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+#include<unistd.h>
 
 void main(int argc , char *argv[])
 {
                FILE *fp;
 	       int flag = 0;
-               char line[600];
+               char line[300];
 
               // initialsing the file pointer to read
               fp = fopen(argv[2],"r");
@@ -17,6 +18,8 @@ void main(int argc , char *argv[])
                      {
                              // print that line
                              printf("%s\n" , line);
+			     //line[strlen(line)-1]='\n';
+			     //write(STDOUT_FILENO,line,strlen(line));
 			     flag = 1;
                       }
                      else
